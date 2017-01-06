68f86437c7d8d7ba2b51fa79f28a3264559571e9
Will Lima <wildoidao@gmail.com>
2017-01-06 11:20:50 -0200

    Update instador_rasp.sh
    Criada a função atualiza_sistema

--- instador_rasp.sh
+++ instador_rasp.sh
@@ -5,22 +5,42 @@ function adiciona_repositorios {
   sudo echo "deb-src http://www.deb-multimedia.org jessie main non-free" >>  /etc/apt/sources.list
 }
 
+function atualiza_sistema {
+  echo "     "
+  echo "Atualizando (update)....."
+  echo "  "
+  sudo apt-get update
+ 
+  echo "     "
+  echo "Atualizando (upgrade) ......."
+  echo "  "
+  sudo apt-get upgrade
+  
+  echo "     "
+  echo "Atualizando (rpi)....."
+  echo "  "
+  sudo rpi-update
+  
+  echo "     "
+  echo "Fim das atualizacoes passando para dependencias"
+  echo "  "
+}
+
 function install_build_tools {
 
   echo "     "
-  echo "Instalando dependencias e atualziando o sistema"
+  echo "Instalando dependencias sistema"
   echo "  "
+  sudo apt-get install deb-multimedia-keyring
   sudo apt-get update
-	sudo apt-get install deb-multimedia-keyring
-	sudo apt-get update
-	sudo apt-get remove ffmpeg
-	sudo apt-get install build-essential libmp3lame-dev libvorbis-dev libtheora-dev libspeex-dev yasm pkg-config libfaac-dev libopenjpeg-dev libx264-dev
+  sudo apt-get remove ffmpeg
+  sudo apt-get install build-essential libmp3lame-dev libvorbis-dev libtheora-dev libspeex-dev yasm pkg-config libfaac-dev libopenjpeg-dev libx264-dev
 }
 
 
 function baixando_ffmpeg {
 	
-	echo "Baixando ffmpeg....."
+  echo "Baixando ffmpeg....."
   mkdir software
   cd software
   wget http://ffmpeg.org/releases/ffmpeg-2.7.2.tar.bz2
@@ -32,18 +52,19 @@ function baixando_ffmpeg {
 
 function compilacao_utilizacao {
 	
-	echo "Compilando ffmpeg....."
+  echo "Compilando ffmpeg....."
   cd ffmpeg- 2.7.2
   ./configure --enable-gpl --enable-postproc --enable-swscale --enable-avfilter --enable-libmp3lame --enable-libvorbis --enable-libtheora --enable-libx264 --enable-libspeex - permitam-compartilhada --enable-pthreads --enable-libopenjpeg --enable-libfaac --enable-nonfree 
   make 
   sudo make install
 
   echo "Executando ....."
-  / sbin / ldconfig
+  sudo /sbin/ldconfig
   
 }
 
 adiciona_repositorios
+atualiza_sistema
 install_build_tools
 baixando_ffmpeg
 compilacao_utilizacao
